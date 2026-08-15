88d1ca33797c357a9bf38cbf72a1bbd956730d39
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-08-16 04:11:09 +0900

    05

--- main.c
+++ main.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void square_array(int a[], int size);
 void print_array(int a[], int size);
 
+void addMatrix(int A[][COLS], int B[][COLS], int c[][COLS])
+	{
+		int i,j;
+		for (i=0; i<ROWS; i++)
+			for (j=0; j<COLS; j++)
+				c[i][j] = A[i][j]+B[i][j];
+	}
+void printMatrix(int A[][COLS])
+{
+	int i,j;
+	for (i=0; i<ROWS; i++)
+	{
+		for (j=0;j<COLS;j++)
+		printf("%3d", A[i][j]);
+		printf("\n");
+	}
+}
+
+
 int main(void)
 {
-	int list[SIZE] = {1,2,3,4};
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
+	int A[ROWS][COLS]={
+	{1,2,3},
+	{5,6,7},
+	{7,8,9}};
+	int B[ROWS][COLS]= {
+	{1,2,4},
+	{0,1,3},
+	{1,0,0}};
+	int C [ROWS][COLS];
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
 	return 0;
+	
 }
-void square_array(int a[], int size)
-{
-	int i;
-	for (i=0; i<size; i++)
-		 a[i] = a[i]*a[i];
-}
-
-void print_array(int a[], int size)
-{
-	int i;
-		for (i=0; i<size; i++)
-		printf("%3d", a[i]);
-	printf("\n");
-}
-
